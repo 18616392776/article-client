1f4efe773ea2ba26402e1b0edfd55d1492d7e904
tanbo <464328895@qq.com>
2017-08-09 16:16:35 +0800

    提供相关文档

--- server/product-server.js
+++ server/product-server.js
@@ -1,10 +1,26 @@
 const historyApiFallback = require("connect-history-api-fallback");
 const favicon = require('express-favicon');
 const express = require('express');
+const globalConfig = require('../global.config');
 
 const app = express();
 
-const globalConfig = require('../global.config');
+
+app.use('/api', httpProxyMiddleware({
+    target: proxyConfig.api[0],
+    pathRewrite: {
+        '^/api/': '/'
+    },
+    onProxyReq(proxyRequest, request, response) {
+        console.log(`请求：${request.url}，代理到${proxyConfig.api[1]}：${proxyConfig.api[0]}${proxyRequest.path}`)
+    }
+}));
+app.use('/images-lib', httpProxyMiddleware({
+    target: proxyConfig.api[0],
+    onProxyReq(proxyRequest, request, response) {
+        console.log(`请求：${request.url}，代理到${proxyConfig.api[1]}：${proxyConfig.api[0]}${proxyRequest.path}`)
+    }
+}));
 
 const appPort = globalConfig.productionPort;
 const appBasePath = globalConfig.buildPath;
